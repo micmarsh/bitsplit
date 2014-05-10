983d7d20072cee535b7aa91f0b14734a06b4922d
micmarsh <michael.marsh42@gmail.com>
2014-05-10 16:19:09 -0400

    move mock stuff into its own ns

--- src/bitsplit/core.clj
+++ src/bitsplit/core.clj
@@ -1,55 +1,5 @@
-(ns bitsplit.core
-    (:require [clj-btc.core :as btc]))
-
-(def all-unspent 
-    #(btc/listunspent 
-        :minconf 0
-        :maxconf 9999999))
-
-(defn nrand [start end]
-    (-> (- end start)
-        rand
-        (Math/floor)
-        (+ start)))
-
-(def rbool #(-> (rand) (< 0.3)))
-
-(defn random-percentages 
-    ([] 
-        (let [new-amt (nrand 0 100)]
-            (random-percentages 
-                [new-amt]
-                (- 100 new-amt))))
-    ([created remaining]
-        (if (or (= remaining 0) (rbool) )
-            (conj created remaining)
-            (let [new-amt (nrand 0 remaining)]
-                (recur 
-                    (conj created new-amt)
-                    (- remaining new-amt))))))
-
-(defn combine-pair [map [addr per]]
-    (if (contains? map addr)
-        (let [amount (map addr)]
-            (assoc map addr (+ amount per)))
-        (assoc map addr per)))
-(def pairs->map #(reduce combine-pair { } %))
-
-(defn sample-divisions [addresses]
-    (let [;addresses (map #(% "address") (all-unspent))
-          infaddr (cycle addresses)
-          pairs (map (fn [addr per] [addr (/ per 100)]) infaddr (random-percentages))]
-        (pairs->map pairs)))    
-
-(defn addr-seq [] (map #(% "address") (all-unspent)))
-
-(defn full-division []
-    (let [all (addr-seq)]
-        (pairs->map (map (fn [addr]
-            (let [addresses (set (addr-seq))
-                  other (disj addresses addr)]
-                  [addr (sample-divisions other)]))
-        all))))
+(ns bitsplit.core)
+    
 
 (defn foo
   "I don't do a whole lot."
