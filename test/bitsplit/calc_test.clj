9a83944c6c954b2c8080d502d0111b69ea0956a2
micmarsh <michael.marsh42@gmail.com>
2014-05-18 14:51:33 -0400

    get rid of midje apply-diff tests

--- test/bitsplit/calc_test.clj
+++ test/bitsplit/calc_test.clj
@@ -7,29 +7,6 @@
         [clojure.test.check.properties :as prop] 
         [bitsplit.calculate :as calc]))
 
-(fact "base case returns empty"
-    (calc/apply-diff 0.2M { }) => { })
-
-(def mono-base {"hello" 0.07M})
-
-(fact "add and subtract to single case"
-    (calc/apply-diff 0.3M mono-base) 
-        => {"hello" 0.37M}
-    (calc/apply-diff -0.04M mono-base)
-        => {"hello" 0.03M})
-
-(def double-base {"hello" 0.4M "yo" 1.2M})
-(def triple-base (conj double-base ["heyyo" 0.5M]))
-
-(fact "can handle actually dividing things"
-    (calc/apply-diff 0.6M double-base)
-        => {"hello" 0.7M "yo" 1.5M}
-    (calc/apply-diff 0.93M triple-base)
-        => {
-            "hello" 0.71M "yo" 1.51M
-            "heyyo" 0.81M
-            })
-
 (def gen-decimal (->> gen/s-pos-int
                     (gen/fmap (comp str float #(/ % 100)))
                     (gen/fmap #(java.math.BigDecimal. %))))
