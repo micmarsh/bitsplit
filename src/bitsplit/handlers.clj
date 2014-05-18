c4d764f9e603993341750936325075c11c2811ce
micmarsh <michael.marsh42@gmail.com>
2014-05-18 16:59:12 -0400

    forgot how to protocol

--- src/bitsplit/handlers.clj
+++ src/bitsplit/handlers.clj
@@ -1,27 +1,30 @@
 (ns bitsplit.handlers
-    (:use 
-        [bitsplit.calculate :only (save-split delete-split)]
+    (:use
         bitsplit.storage.protocol
-        bitsplit.storage.filesystem))
+        bitsplit.storage.filesystem)
+    (:require [bitsplit.calculate :as calc]))
+
+(defprotocol Finishable
+    (finish [this data]))
 
 (defrecord BalancedFile [data location persist?]
-    
-    (finish [this new-splits]
-        (when persist?
-            (spit location new-splits))
-        (assoc this :data new-splits)))
-
     IStorage
     (all [this]
         (if persist? 
             (try-file location data)
             data))
     (split! [this from to percentage]
-        (let [new-splits (save-split data from to percentage)]
-            (finish this new-splits))
-    (unsplit! [this from to]
-        (let [new-splits (delete-split data from to)]
+        (let [new-splits (calc/save-split data from to percentage)]
             (finish this new-splits)))
+    (unsplit! [this from to]
+        (let [new-splits (calc/delete-split data from to)]
+            (finish this new-splits)))
+
+    Finishable
+    (finish [this new-splits]
+        (when persist?
+            (spit location new-splits))
+        (assoc this :data new-splits)))
 
 (def storage 
     (map->BalancedFile
