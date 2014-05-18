1887488704b00ed12dfe9f55fbc9da23b6fa79e4
micmarsh <michael.marsh42@gmail.com>
2014-05-18 19:46:56 -0400

    getting cljs ready!

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/test.check "0.5.8"]
+  :dependencies [[org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/test.check "0.5.8"]
+                 [org.clojure/data.json "0.2.4"]
                  [org.clojure/clojure "1.5.1"]
                  [compojure "1.1.8"]
+                 [reagent "0.4.2"]
                  [midje "1.6.3"]
                  [ring "1.2.2"]
 
@@ -14,5 +18,13 @@
                  [http-kit "2.1.11"]]
 
   :plugins [[lein-ring "0.7.1"]
-            [lein-midje "3.0.0"]]
-  :ring {:handler bitsplit.core/app})
+            [lein-midje "3.0.0"]
+            [lein-cljsbuild "1.0.2"]]
+
+  :ring {:handler bitsplit.core/app}
+  :cljsbuild
+      {:builds [{:source-paths ["src/client"]
+                   :compiler
+                     {:preamble ["reagent/react.js"]
+                      :output-to "resources/client/main.js"
+                      :pretty-print true}}]})
--- src/bitsplit/core.clj
+++ src/bitsplit/core.clj
@@ -2,7 +2,8 @@
   (:use compojure.core
         [ring.middleware.params :only (wrap-params)])
   (:require [compojure.route :as route]
-            [bitsplit.handlers :as handlers]))
+            [bitsplit.handlers :as handlers]
+            [clojure.data.json :as json]))
 
 (defroutes app-routes
     (GET "/splits" [] 
@@ -10,7 +11,6 @@
     (POST "/splits/:from/:to" [] handlers/save!)
     (DELETE "/splits/:from/:to" [] handlers/delete!)
     (route/not-found "<h1>Page not found</h1>"))
-
-
+; really should use liberator
 (def app (-> app-routes
             wrap-params))
