fe47e07b81f25a97f0e4f97120769482f427e657
micmarsh <mike@marsh.pw>
2014-06-03 21:47:24 -0400

    dev profile

--- project.clj
+++ project.clj
@@ -4,24 +4,28 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [org.clojure/test.check "0.5.8"]
                  [org.clojure/data.json "0.2.4"]
                  [org.clojure/clojure "1.5.1"]
-                 [fluyt "0.1.0-SNAPSHOT"]
                  [marshmacros "0.2.1"]
                  [compojure "1.1.8"]
-                 [reagent "0.4.2"]
-                 [digest "1.4.4"]
-                 [midje "1.6.3"]
                  [ring "1.2.2"]
                  ;[clj-btc "0.1.1"]
                  [http-kit "2.1.11"]]
 
-  :plugins [[lein-ring "0.7.1"]
-            [lein-midje "3.0.0"]
-            [lein-cljsbuild "1.0.2"]
-            [com.keminglabs/cljx "0.3.2"]]
+  :profiles {
+    :dev {
+        :dependencies [[org.clojure/clojurescript "0.0-2173"]
+                       [org.clojure/test.check "0.5.8"]
+                       [fluyt "0.1.0-SNAPSHOT"]
+                       [reagent "0.4.2"]
+                       [midje "1.6.3"]]
+        :plugins [[lein-ring "0.7.1"]
+                  [lein-midje "3.0.0"]
+                  [lein-cljsbuild "1.0.2"]
+                  [com.keminglabs/cljx "0.3.2"]]
+
+      }
+  }
 
   :main bitsplit.core
 
