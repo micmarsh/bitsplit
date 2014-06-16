320134dc5d041dd93db04ffdb960d62c9bb6c0ff
micmarsh <mike@marsh.pw>
2014-06-15 20:07:06 -0400

    non working almost implementation of bitcoinj record

--- src/bitsplit/clients/bitcoinj.clj
+++ src/bitsplit/clients/bitcoinj.clj
@@ -1,1 +1,26 @@
-(ns bistplist.clients.bitcoinj)
+(ns bitsplit.clients.bitcoinj
+    (:use bitcoin.core
+          clojure.core.async
+          bitsplit.clients.protocol))
+
+(def emap (comp doall map))
+
+(defrecord Bitcoinj [wallet]
+    BitsplitClient
+    (unspent-amounts [this] { })
+    (unspent-channel [this]
+        (let [return (chan)]
+            (on-coins-received wallet
+                (fn [tx prev balance]
+                    (put! return nil)))))
+    (send-many! [this amounts]
+        (emap 
+            (fn [[to amount]]
+                (send-coins wallet to amount))
+                    amounts))
+    (new-address! [this]
+        (let [kp (create-keypair)]
+            (kp->address kp))))
+
+
+
