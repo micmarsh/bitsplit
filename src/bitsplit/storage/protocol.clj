cfe9fc90ced0713c12f1a466b10e6ef718827f1e
micmarsh <mike@marsh.pw>
2014-07-02 16:42:55 -0400

    docs and better name for storage

--- src/bitsplit/storage/protocol.clj
+++ src/bitsplit/storage/protocol.clj
@@ -1,6 +1,9 @@
 (ns bitsplit.storage.protocol)
 
-(defprotocol IStorage
-    (all [store])
-    (unsplit! [store from to])
-    (split! [store from to percentage]))
+(defprotocol Storage
+    (all [store]
+        "List every split: {\"split1\" {\"addr1\" percent ...} ...}")
+    (unsplit! [store from to]
+        "Remove the given from - to address pair, returning the new split or an empty map")
+    (split! [store from to percentage]
+        "Add the given from - to pair, returning the new split"))
