129f69c1fafaeaf49dda2dc27b41f758a1c95b54
micmarsh <mike@marsh.pw>
2014-07-02 12:45:18 -0400

    "handlers" was horribly named and should be part of core

--- src/bitsplit/core.clj
+++ src/bitsplit/core.clj
@@ -4,6 +4,22 @@
   (:require [bitsplit.handlers :as handlers]
             [bitsplit.transfer :as transfer]))
 
+(defn save! [storage params]
+    (let [{:keys [from to percentage]} params]
+        (->> (java.math.BigDecimal. percentage)
+            (swap! storage split! from to)
+            :data)))
+
+(defn handle-actions! [storage actions changes]
+    (go (while true
+        (let [action (<! actions)]
+            (condp = (:type action)
+                :add-address
+                    (put! changes
+                        {:percentages ((save! storage action)
+                                            (:from action))
+                         :from (:from action)
+                         :type :add-address}))))))
 
 (defn -main [ & [mode] ]
     (println "sup")) 
