6330c0c220cd39e81f60e9b35555470c1a73cb6b
micmarsh <mike@marsh.pw>
2014-07-02 12:35:52 -0400

    main namespace is going to be more main-y

--- src/bitsplit/core.clj
+++ src/bitsplit/core.clj
@@ -1,28 +1,9 @@
 (ns bitsplit.core
-  (:use bitsplit.clients.bitcoind
-        bitsplit.storage.filesystem
+  (:use  bitsplit.storage.filesystem
         bitsplit.clients.protocol)
   (:require [bitsplit.handlers :as handlers]
-            [bitsplit.transfer :as transfer]
-            [bitsplit.views.main :as ui]))
+            [bitsplit.transfer :as transfer]))
 
-(defn mapmap [fn seq & others]
-    (into { } (apply map fn seq others)))
-
-(defn make-storage [client]
-    (-> {:data (mapmap (fn [addr] [addr { }]) (addresses client)) ; less neccesary once this is reading from persistence
-         :location SPLITS_LOCATION
-         :persist? false}
-        map->BalancedFile))
 
 (defn -main [ & [mode] ]
-        (let [client (->Bitcoind "")
-              storage (atom (make-storage client))
-              changes (clojure.core.async/chan)
-              unspents (unspent-channel client)]
-            (if (= mode "headless")
-                (println "Starting Bitsplit Process...")
-                (let [actions (ui/start-ui! (handlers/list-all storage) changes)]
-                  (println "Starting Bitsplit UI...")
-                  (handlers/handle-actions! storage actions changes)))
-            (transfer/handle-unspents! client storage unspents))) 
+    (println "sup")) 
