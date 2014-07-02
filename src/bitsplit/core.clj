3ebc2ace86b419501e671c1081064170ee0c25ff
micmarsh <mike@marsh.pw>
2014-07-02 17:01:36 -0400

    make the storage protocol less granular

--- src/bitsplit/core.clj
+++ src/bitsplit/core.clj
@@ -3,30 +3,36 @@
         bitsplit.clients.protocol
         [clojure.core.async :only (go put! <!)])
   (:require [bitsplit.handlers :as handlers]
-            [bitsplit.transfer :as transfer]))
+            [bitsplit.transfer :as transfer]
+            [bitsplit.utils.calculate :as calc]))
 
-(defn save! [storage params]
-    (let [{:keys [from to percentage]} params]
-        (->> (java.math.BigDecimal. percentage)
-            (swap! storage split! from to)
-            :data)))
+; (defn save! [storage params]
+;     (let [{:keys [from to percentage]} params]
+;         (->> (java.math.BigDecimal. percentage)
+;             (swap! storage split! from to)
+;             :data)))
 
-(defn handle-actions! [storage actions changes]
-    (go (while true
-        (let [action (<! actions)]
-            (condp = (:type action)
-                :add-address
-                    (put! changes
-                        {:percentages ((save! storage action)
-                                            (:from action))
-                         :from (:from action)
-                         :type :add-address}))))))s
+; (defn handle-actions! [storage actions changes]
+;     (go (while true
+;         (let [action (<! actions)]
+;             (condp = (:type action)
+;                 :add-address
+;                     (put! changes
+;                         {:percentages ((save! storage action)
+;                                             (:from action))
+;                          :from (:from action)
+;                          :type :add-address}))))))
 
-(defn add-address! [storage {:keys [parent address percent]}])
+(defn add-address! [storage {:keys [parent address percent]}]
+    (let [existing (lookup storage parent)
+          new-split (calc/save-percentage existing address percent)]
+        (save! storage parent new-split)))
 
 (defn remove-address! [storage {:keys [parent address]}])
 
-(defn new-split! [storage client {:keys [children]}])
+(defn new-split! 
+    ([storage client] (new-split! storage client { }))
+    ([storage client {:keys [children]}]))
 
 (defn remove-split! [storage {:keys [split]}])
 
--- src/bitsplit/storage/protocol.clj
+++ src/bitsplit/storage/protocol.clj
@@ -3,7 +3,9 @@
 (defprotocol Storage
     (all [store]
         "List every split: {\"split1\" {\"addr1\" percent ...} ...}")
-    (unsplit! [store from to]
+    (lookup [this split]
+        "Returns the {\"addr1\" percent ...} listing for a given split")
+    (delete! [store address]
         "Remove the given from - to address pair, returning the new split or an empty map")
-    (split! [store from to percentage]
-        "Add the given from - to pair, returning the new split"))
+    (save! [store address splits]
+            "Save the given set of splits and return {\"split\" {\"addr1\" percent ...}}"))
